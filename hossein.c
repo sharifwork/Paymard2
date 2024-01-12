5a6b5a54ef4c6aaa7a76a9811c9dd9f75686b16c
sharifwork <152766157+sharifwork@users.noreply.github.com>
2024-01-12 17:21:27 +0330

    edit hossein.c
    zoj ya fard

--- hossein.c
+++ hossein.c
@@ -1,8 +1,15 @@
 include<iostream>
+using namespace std;
 
 int main(){
 
-  cout << "salam hadi";
-  cout << "Salam Hossein";
+	cout << "zoj ya fard\n";
+	int n;
+	cin >> n;
+	if(n%2)
+		cout << "fard";
+	else
+		cout << "zoj";
+
   return 0;
 }
